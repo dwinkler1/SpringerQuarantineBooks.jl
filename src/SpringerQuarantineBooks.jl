7086882b388cfcc7cc337b51aa294d427089eb8a
Daniel Winkler <danielw2904@disroot.org>
2020-05-06 18:03:19 +0200

    added epub support

--- src/SpringerQuarantineBooks.jl
+++ src/SpringerQuarantineBooks.jl
@@ -1,6 +1,6 @@
 module SpringerQuarantineBooks
 
-    import HTTP 
+    import HTTP
     import XLSX
     import DataFrames
     import ProgressMeter
@@ -12,14 +12,14 @@ module SpringerQuarantineBooks
         if(a[end] == '/' || b[1] == '/')
             out = a * b
         else
-            out = a * '/' * b 
+            out = a * '/' * b
         end
     end
 
     function getbooklist(path = pwd(); name = "booklist.xlsx")
         fname = makepath(path, name)
         if(!isfile(fname))
-            bookurl = "https://resource-cms.springernature.com/springer-cms/rest/v1/content/17858272/data/v5/"
+            bookurl = "https://resource-cms.springernature.com/springer-cms/rest/v1/content/17858272/data/"
             req = HTTP.get(bookurl)
             open(fname, "w") do io
                 write(io, req.body)
@@ -34,17 +34,23 @@ module SpringerQuarantineBooks
         return df
     end
 
-    function loadbooks(path = pwd(), booklist = getbooklist(path); fixnames = true, verbose = true)
+    function loadbooks(path = pwd(), booklist = getbooklist(path); format = "pdf", fixnames = true, verbose = true, printerrors = (format == "pdf"))
+        format = lowercase(format)
+        @assert format ∈ ["pdf", "epub"] """ Valid formats are "pdf" and "epub" """
         fixednames = replace.(string.(names(booklist)), r"\s" => "_")
         bl = DataFrames.rename(booklist, names(booklist) .=> Symbol.(fixednames))
         subjects = unique(bl.English_Package_Name)
-        
+
         fixnames && (fixer = r"[,\s/:]+")
 
-        baseurl = "https://link.springer.com/content/pdf"
-        
+        if format == "pdf"
+            baseurl = "https://link.springer.com/content/pdf"
+        elseif format == "epub"
+            baseurl = "https://link.springer.com/download/epub"
+        end
+
         verbose && (progress = ProgressMeter.Progress(size(bl, 1); desc = "Downloading"))
-    
+
         for i in eachrow(bl)
             url = i.OpenURL
             if(fixnames)
@@ -52,24 +58,24 @@ module SpringerQuarantineBooks
                 book = replace(i.Book_Title, fixer => "_")
                 isbn = replace(i.Electronic_ISBN, fixer => "_")
             else
-                folder = replace(i.English_Package_Name, r"[/:]+" => "_") 
+                folder = replace(i.English_Package_Name, r"[/:]+" => "_")
                 book = replace(i.Book_Title, r"[/:]+" => "_")
                 isbn = replace(i.Electronic_ISBN, r"[/:]+" => "_")
             end
             isdir(makepath(path, folder)) || mkdir(makepath(path, folder))
-            fname = folder * '/' * book * '_' * isbn * ".pdf"
+            fname = folder * '/' * book * '_' * isbn * '.' * format
             isfile(fname) && continue
-            
+
             try
                 res = HTTP.get(url)
-                dlurl = replace(res.request.target, r"\%2F|/book/" => '/') * ".pdf"
+                dlurl = replace(res.request.target, r"\%2F|/book/" => '/') * '.' * format
                 open(makepath(path, fname), "w") do io
                     book = HTTP.get(baseurl * dlurl)
                     write(io, book.body)
                 end
                 verbose && ProgressMeter.next!(progress; showvalues = [(Symbol("Book name"), i.Book_Title), (:Folder, i.English_Package_Name)])
             catch e
-                println("Error: ", e, "when loading", url)
+                printerrors && println("Error: ", e, " when loading: ", url)
             end
         end
     end
